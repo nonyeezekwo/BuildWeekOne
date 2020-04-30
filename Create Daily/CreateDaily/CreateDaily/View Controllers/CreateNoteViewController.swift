3e4f85dea1ea81612eaca457fb350b233b194d4e
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-04-30 16:02:33 -0700

    UI impemented changes

--- Create Daily/CreateDaily/CreateDaily/View Controllers/CreateNoteViewController.swift
+++ Create Daily/CreateDaily/CreateDaily/View Controllers/CreateNoteViewController.swift
@@ -18,22 +18,31 @@ class CreateNoteViewController: UIViewController {
     //MARK: - OUTLETS & ACTIONS
     @IBOutlet weak var noteTitleTextField: UITextField!
     @IBOutlet weak var noteTextView: UITextView!
+    @IBOutlet weak var noteSaveButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureNoteButton()
     }
     //MARK: - NOTIFICATION OF SAVE BUTTON TAPPED
     @IBAction func saveNoteTapped(_ sender: Any) {
         guard let noteTitle = noteTitleTextField.text,
             let noteDescription = noteTextView.text else { return }
         delegate?.noteWasCreated(Notes(noteTitle: noteTitle, noteDescription: noteDescription))
-        let alert = UIAlertController(title: "Note Created!", message: "Your note entry is signed sealed & saved", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Note Created!", message: "Your new note has been saved", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "Finished", style: .default){
             (UIAlertAction) -> Void in
             self.navigationController?.popToRootViewController(animated: true)
         })
     present(alert, animated: true, completion: nil)
 }
+    func configureNoteButton() {
+        noteSaveButton.layer.borderWidth = 3
+        noteSaveButton.layer.borderColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        noteSaveButton.layer.cornerRadius = 20
+        noteSaveButton.layer.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+    }
     
     @IBAction func noteReminderTapped(_ sender: Any) {
     }
